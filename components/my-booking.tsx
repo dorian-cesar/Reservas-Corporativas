d554b114f35d0c9c4b6ebd849aaec866fc560c34
diego <dwigodski@gmail.com>
2025-12-10 10:59:53 -0300

    fix: se modifican estilos de íconos en reservas de viajes

--- components/my-booking.tsx
+++ components/my-booking.tsx
@@ -689,9 +689,9 @@ export function MyBookings({
                     }
                   >
                     {booking.ticketStatus?.toLowerCase() === "confirmed" ? (
-                      <CheckCircle2 className="h-3 w-3 mr-1" />
+                      <CheckCircle2 className="h-3 w-3" />
                     ) : (
-                      <XCircle className="h-3 w-3 mr-1" />
+                      <XCircle className="h-3 w-3" />
                     )}
                     {booking.ticketStatus.toLowerCase() === "confirmed"
                       ? "Confirmado"
